0a274ca6030b7740d55e1d3ac654c94f240e5c73
Steve Holden <steve@holdenweb.com>
2016-03-17 13:05:38 +0000

    Removed redundant printing, make transitive dependency computation work

--- dependencies.py
+++ dependencies.py
@@ -30,11 +30,14 @@ class Module:
     @property
     def trans_deps(self):
         if not self.trans_done:
-            print("Computing transitive dependencies for", self.name)
+            #print("Computing transitive dependencies for", self.name)
             for dep in self._deps:
-                self._trans_deps.add(dep)
+                if dep in dep_dict:
+                    module = dep_dict[dep]
+                    self._trans_deps = self._trans_deps.union(module.trans_deps)
             self.trans_done = True
-        return self._trans_deps
+            #print(self.name, "transitive dependencies done")
+        return self._trans_deps.union(self._deps)
     def provides(self, dep):
         if dep in self.trans_reps({}):
             return True
@@ -56,32 +59,25 @@ def get_deps():
         deps = glob(os.path.join("repos", repo, "requirements*.txt"))
         dep_dict[repo] = repo_dep = Module(repo)
         for dep in deps:
+            #print("Processing", dep)
             for line in lines_of(open(dep)):
+                if line.startswith("-e ."):
+                    continue
+                if line.startswith("-e git+git@github.com:bmlltech"):
+                    #print("Extras on", dep)
+                    _, line = line.split("=")
                 repo_dep.add_dep(line)
     return dep_dict
 
 if __name__ == "__main__":
     dep_dict = get_deps()
+    for name, module in dep_dict.items():
+        module.trans_deps
 
     #for repo in sorted(dep_dict):
         #print("===", repo, "===")
         #for dep in sorted(dep_dict[repo].deps):
             #print('\t', dep)
-    
-    print("+++++++++++++First round++++++++++++++++++")
-    
-    for repo in dep_dict:
-        dep_dict[repo].trans_deps
-    
-    print("+++++++++++++Second round++++++++++++++++++")
-    
-    for repo in dep_dict:
-        dep_dict[repo].trans_deps
-    
-    print("+++++++++++++Third round++++++++++++++++++")
-    
-    for repo in dep_dict:
-            dep_dict[repo].trans_deps
     
     
     for repo in sorted(dep_dict):
@@ -89,6 +85,11 @@ if __name__ == "__main__":
         for dep in sorted(dep_dict[repo]._deps):
             print('\t', dep)
     
-    
-    
+    all_modules = set()
+    for name, module in dep_dict.items():
+        all_modules = all_modules.union(module.trans_deps)
+
+    print("\n\n\n----- All  modules -----\n\n")
+    for name in sorted(all_modules):
+        print(name)
     print("Finished")
